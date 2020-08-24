addc03a5bebc546e8037348cb5e20c31544c26b5
rikoprogrammer <erikodongo.35@gmail.com>
2020-08-24 08:23:39 +0300

    Update R files

--- Simple linear regressor.R
+++ Simple linear regressor.R
@@ -1,14 +1,43 @@
 #Linear regression
-install.packages("caTools")
-library(caTools)
-str(cars)
-my_data = cars
-ratio = sample.split(my_data, SplitRatio = 0.8)
-train = subset(my_data, ratio == TRUE)
-test = subset(my_data, ratio == FALSE)
-regressor = lm(speed ~ dist, data = train)
-summary(regressor)
-y_pred = predict(regressor, test)
-comparisons = cbind(test$speed, y_pred)
-comparisons
+#Importing the dataset
+dataset = read.csv("My_data.csv")
 
+#Splitting data into training and test sets
+#install.packages("caTools")
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+
+#fitting a simple linear model
+regressor = lm(formula = Salary ~ YearsExperience, 
+               data = training_set)
+
+#predict the test results
+y_pred = predict(regressor, newdata = test_set)
+
+# Visualising the Training set results
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('Salary vs Experience (Training set)') +
+  xlab('Years of experience') +
+  ylab('Salary')
+
+# Visualising the Test set results
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('Salary vs Experience (Test set)') +
+  xlab('Years of experience') +
+  ylab('Salary')
